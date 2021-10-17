3f78ba705b36f1d9752ecf489cd36f056453f77e
Cologler <skyoflw+github@gmail.com>
2021-10-17 21:19:00 +0800

    feat: support bat/cmd

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
-use std::path::Path;
-use std::process::{Command, exit};
+use std::error::Error;
+use std::path::PathBuf;
+use std::process::{Command, ExitStatus, exit};
 
-fn main() {
-    let curexe = std::env::current_exe().unwrap();
-    let exepath = Path::new(curexe.to_str().unwrap());
-    let script = exepath.with_extension("ps1");
-    if script.is_file() {
-        let mut cmd = Command::new("powershell");
-        cmd.arg("-ExecutionPolicy").arg("Bypass");
-        cmd.arg("-File").arg(script);
-        cmd.args(std::env::args().skip(1));
-        let es = cmd.spawn().unwrap().wait().unwrap();
-        if !es.success() {
-            std::io::stdin().read_line(&mut String::new()).unwrap_or_default();
-        }
-        exit(es.code().unwrap_or(0));
+fn run_ps1(script: &PathBuf) -> Result<ExitStatus, Box<dyn Error>> {
+    let mut cmd = Command::new("powershell");
+    cmd.arg("-NoProfile");
+    cmd.arg("-ExecutionPolicy").arg("Bypass");
+    cmd.arg("-File").arg(script);
+    cmd.args(std::env::args().skip(1));
+    let es = cmd.spawn()?.wait()?;
+    Ok(es)
+}
+
+fn run_cmd(script: &PathBuf) -> Result<ExitStatus, Box<dyn Error>> {
+    let mut cmd = Command::new("cmd");
+    cmd.arg("/C").arg(script);
+    cmd.args(std::env::args().skip(1));
+    let es = cmd.spawn()?.wait()?;
+    Ok(es)
+}
+
+fn find_file(base: &PathBuf, ext: &str) -> Option<PathBuf> {
+    let file = base.with_extension(ext);
+    return if file.exists() {
+        Some(file)
     } else {
-        println!("Missing {} file", script.to_str().unwrap());
+        None
+    };
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let curexe = std::env::current_exe()?;
+    let exepath = PathBuf::from(curexe.to_str().unwrap());
+
+    let es = if let Some(ps1file) = find_file(&exepath, "ps1") {
+        run_ps1(&ps1file)?
+    } else if let Some(batfile) = find_file(&exepath, "bat") {
+        run_cmd(&batfile)?
+    } else if let Some(batfile) = find_file(&exepath, "cmd") {
+        run_cmd(&batfile)?
+    } else {
+        Err("Missing script file")?
+    };
+
+    if !es.success() {
+        std::io::stdin().read_line(&mut String::new()).unwrap_or_default();
     }
+    exit(es.code().unwrap_or(0));
 }
